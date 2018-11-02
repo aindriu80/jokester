5aa2381a022153642b0037e2412dc90aecc7831c
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-11-02 21:48:56 +0000

    Adding one joke at a time

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,25 @@
 <template>
     <div id="app">
+        <h3>Got jokes?</h3>
     <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <button class="btn btn-primary" @click="addJoke">Add A Joke</button>
     <br><br>
-    <div class="col-md-12">
-        <Joke 
-        v-for="(joke, index) in $store.state.jokes"
-        :joke="joke"
-        key="index"
-        />
-    </div>        
+        <div class="col-md-12">
+            <Joke 
+            v-for="(joke, index) in $store.state.jokes" 
+            :joke="joke" 
+            key="index" 
+            />
+        </div>        
     </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 import Joke from "./Joke.vue";
+
 export default {
-  methods: mapActions(["initJokes"]),
+  methods: mapActions(["initJokes", "addJoke"]),
   components: { Joke }
 };
 </script>
